24baa2baf80569f43b4680b4af22092fd76b8f8b
Thang Nguyen <thangdn@thanglong.edu.vn>
2018-02-01 17:49:54 +0700

    update ex1

--- ex1.cpp
+++ ex1.cpp
@@ -1,22 +1,25 @@
-bhbkjbfkjbfkjbjkl
- kjvnokbsl;\hijsdb;ld
+#include <iostream>
+#include <math.h>
+using namespace std;
 
- vjhbiofemnkf\hdghef;
+double babylonianRoot(double n);
 
- dnvoijf'
+int main()
+{
+	int n;
+	char h;
+	do{
+		cout<<"enter a value: ";
+		cin>>n;
+		cout<<"square root of "<<n<<" is "<<babylonianRoot(n)<<endl;
+		cout<<"continue? (y/n): ";
+		cin>>h;
+	}while(h != 'n');
+	
+	return 0;
+}
 
-
- hhufbnfel
-
-
- hudhb
- .begin(), 
-
- dnvoijf'
-
-
- hhufbnfel
-
-
- hudhb
- .end()
+double babylonianRoot(double n)
+{
+	return sqrt(n);
+}
